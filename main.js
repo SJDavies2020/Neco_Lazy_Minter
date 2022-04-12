9b5ba29670773b3b386e687bebb224fd6fffa674
Steven Davies <44582668+SJDavies2020@users.noreply.github.com>
2022-04-12 17:16:23 -0400

    Minter Complete

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-const { base64 }=require("ethers/lib/utils");
-
 const serverUrl="https://wnofl8kyjzv3.usemoralis.com:2053/server"
 const appId="SDTlYXFWMrEKBtZ0rCHzeyUXYf8qAqjJoTCpMg7E";
 
 Moralis.start({ serverUrl, appId });
-
+let user;
 async function login() {
-	let user=Moralis.User.current();
+	user=Moralis.User.current();
 	if (!user) {
 		try {
 			user=await Moralis.authenticate({ signingMessage: "Hello World" })
@@ -16,6 +14,7 @@ async function login() {
 		}
 	}
 	else {
+		Moralis.enableWeb3();
 		initApp();
 		console.log(user);
 		console.log(user.get('ethAddress'))
@@ -24,43 +23,52 @@ async function login() {
 
 function initApp() {
 	document.querySelector("#app").style.display="block";
-	document.querySelector("#submit_button").onclick=submit;
+	document.querySelector("#submit").onclick=submit;
 }
 
 async function submit() {
 
 	// Get Image Data
-	const input=document.querySelector("#input_imge");
-	let data=input.file[0];
+	const input=document.querySelector("#input_image");
+	let data=input.files[0];
 	// Upload to IPFS
 	const imageFile=new Moralis.File(data.name, data);
 	await imageFile.saveIPFS();
 	let imageHash=imageFile.hash();
 	console.log(imageHash);
 	console.log(imageFile.ipfs());
+	document.querySelector("#input_status").value="Status: Uploading Image"
+
 	// Create NFT Meta Data
 	let metadata={
 		name: document.querySelector("#input_name").value,
 		description: document.querySelector("#input_description").value,
 		image: "/ipfs/"+imageHash
 	}
+	document.querySelector("#input_status").value="Status: Creating Meta Data"
 	// upload metadata to IPFS
 	const jsonFile=new Moralis.File("metadata.json", { base64: btoa(JSON.stringify(metadata)) });
 	await jsonFile.saveIPFS();
 	let metadataHash=jsonFile.hash();
-
 	console.log(metadataHash);
-
 	// upload to Rareible
-
-
+	document.querySelector("#input_status").value="Status: Uploading to Rarible!"
+	let res=await Moralis.Plugins.rarible.lazyMint(
+		{
+			chain: 'rinkeby', userAddress: user.get("ethAddress"),
+			tokenType: 'ERC721',
+			tokenUri: '/ipfs/'+metadataHash,
+			royaltiesAmount: 100,
+			// 1% royalty. Optional
+		}
+	)
+	if (res) {
+		document.querySelector("#input_name").value="";
+		document.querySelector("#input_description").value="";
+		document.querySelector("#input_image").value="";
+	}
+	console.log(res);
+	document.querySelector("#input_status").value="Status: Lazy Mint Complete!"
 }
-//async function logout() {
-//	await Moralis.User.logout();
-//	console.log("Logged Out");
-//}
 
 login();
-
-//document.getElementById("btn-login").onclick=login;
-//document.getElementById("btn-logout").onclick=logout;
