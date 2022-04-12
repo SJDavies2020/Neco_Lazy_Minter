3df75fb0c053cabd9401d99f145a49c335e84877
Steven Davies <44582668+SJDavies2020@users.noreply.github.com>
2022-04-12 13:46:51 -0400

    Styleing and IPFS Preperation

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const { base64 }=require("ethers/lib/utils");
+
 const serverUrl="https://wnofl8kyjzv3.usemoralis.com:2053/server"
 const appId="SDTlYXFWMrEKBtZ0rCHzeyUXYf8qAqjJoTCpMg7E";
 
@@ -8,18 +10,51 @@ async function login() {
 	if (!user) {
 		try {
 			user=await Moralis.authenticate({ signingMessage: "Hello World" })
-			console.log(user);
-			console.log(user.get('ethAddress'))
+			initApp();
 		} catch (error) {
 			console.log(error)
 		}
 	}
 	else {
+		initApp();
 		console.log(user);
 		console.log(user.get('ethAddress'))
 	}
 }
 
+function initApp() {
+	document.querySelector("#app").style.display="block";
+	document.querySelector("#submit_button").onclick=submit;
+}
+
+async function submit() {
+
+	// Get Image Data
+	const input=document.querySelector("#input_imge");
+	let data=input.file[0];
+	// Upload to IPFS
+	const imageFile=new Moralis.File(data.name, data);
+	await imageFile.saveIPFS();
+	let imageHash=imageFile.hash();
+	console.log(imageHash);
+	console.log(imageFile.ipfs());
+	// Create NFT Meta Data
+	let metadata={
+		name: document.querySelector("#input_name").value,
+		description: document.querySelector("#input_description").value,
+		image: "/ipfs/"+imageHash
+	}
+	// upload metadata to IPFS
+	const jsonFile=new Moralis.File("metadata.json", { base64: btoa(JSON.stringify(metadata)) });
+	await jsonFile.saveIPFS();
+	let metadataHash=jsonFile.hash();
+
+	console.log(metadataHash);
+
+	// upload to Rareible
+
+
+}
 //async function logout() {
 //	await Moralis.User.logout();
 //	console.log("Logged Out");
